d266272362de6cbd301b5743e9f14c5591513bef
Jan Maaß <git@antipost.de>
2023-10-31 15:55:01 +0100

    Add solution

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  const formElements = event.target.elements;
+  const firstName = formElements.firstName.value;
+
+  const ageBadnessSum = Number(formElements.age.value) + Number(formElements.badness.value);
+
+  console.log(data);
+  console.log(`The age-badness-sum of ${firstName} is ${ageBadnessSum}`);
+  event.target.reset();
+  event.target.elements.firstName.focus();
+})
